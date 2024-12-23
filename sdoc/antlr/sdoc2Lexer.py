77403b41c84910c941e6997a29adbdd01a43f6dd
Paul Water <p.r.water@setbased.nl>
2024-12-23 12:36:47 +0100

    Update dependencies.

--- sdoc/antlr/sdoc2Lexer.py
+++ sdoc/antlr/sdoc2Lexer.py
@@ -1,4 +1,4 @@
-# Generated from sdoc/antlr/sdoc2Lexer.g4 by ANTLR 4.12.0
+# Generated from sdoc/antlr/sdoc2Lexer.g4 by ANTLR 4.13.2
 from antlr4 import *
 from io import StringIO
 import sys
@@ -140,7 +140,7 @@ class sdoc2Lexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.12.0")
+        self.checkVersion("4.13.2")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
